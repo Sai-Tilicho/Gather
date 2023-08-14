759c3baefb1dd6840a86e1c2138559f77d765912
prasad <prasad@tilicho.in>
2023-08-14 12:56:55 +0530

    adding displayConversation module to merge with sparkComponent module

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,5 @@
-import { database } from '@/firbase';
-import React from 'react';
-import { getDatabase, ref, child, get } from "firebase/database";
+import React from "react";
 
 export default function Home() {
-
-  const dbRef = ref(database);
-  get(child(dbRef, `/`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-  return (
-    <div></div>
-  )
+  return <div></div>;
 }
