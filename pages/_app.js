2509aa069e1902a3f4b23a6805e92787cb997ee0
Preeti Das <preeti@tilicho.in>
2023-08-23 12:35:31 +0530

    changes in wrapping component

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import "@/styles/globals.css";
 import { SparkContentContext } from "@/src/components/context/sparkContentContext";
-import { store } from '@/src/components/store'
-import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <SparkContentContext>
-        <Component {...pageProps} />
-      </SparkContentContext>
-    </Provider>
+    <SparkContentContext>
+      <Component {...pageProps} />
+    </SparkContentContext>
   );
 }
